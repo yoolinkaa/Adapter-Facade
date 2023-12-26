c8aee00932671f7739dd5e953e6e923c00632327
yoolinkaa <116589019+yoolinkaa@users.noreply.github.com>
2023-12-26 15:58:57 +0200

    Update Main.java

--- demo/src/main/java/ua/edu/ucu/apps/singleton/Main.java
+++ demo/src/main/java/ua/edu/ucu/apps/singleton/Main.java
@@ -2,7 +2,7 @@ package ua.edu.ucu.apps.singleton;
 
 public class Main {
     public static void main(String[] args) {
-        User user = new User(1, "Yaroslav", "ya.prytula", 18);
+        User user = new User(1, "Iuliia", "yurha.pn", 18);
         user.save();
     }
 }
